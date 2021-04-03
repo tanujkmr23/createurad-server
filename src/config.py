7a2c6db5201571bb6e4ecac032da5d1010a35ab8
Tanuj Kumar <tanujkmr23@gmail.com>
2021-04-03 10:03:40 +0530

    Heroku configuration setup

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ class Development(object):
     """
     DEBUG = True
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://creatsurad:creatsurad@localhost:5432/creatsuraddb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://zipcontyxiniyd:ea273ec958f1091fa79d99625bfec5b5203488e2a1a227c1252c91957c832159@ec2-18-211-97-89.compute-1.amazonaws.com:5432/d2mhlrm03h9r0n'
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 class Production(object):
